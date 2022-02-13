5046b93460cb3e8159ca7edadbbc620ae4bf1cc6
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-02-13 18:09:39 +0200

    Update ProductNotFoundException.java

--- src/main/java/consumer/products/server/product/ProductNotFoundException.java
+++ src/main/java/consumer/products/server/product/ProductNotFoundException.java
@@ -2,13 +2,7 @@ package consumer.products.server.product;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
-        super("Product corresponding to id = " + id + " does not exist");
+        super("Product corresponding to id = " + id + " does not exist" + ResponseEntity.notFound());
     }
-    
-    // #########################
-//    public ProductNotFoundException(Long id) {
-//        super("Product corresponding to id = " + id + " does not exist" + ResponseEntity.notFound());
-//    }
-    // #########################
-    
+ 
 }
