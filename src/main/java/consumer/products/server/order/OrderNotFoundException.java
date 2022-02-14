93ba2f57a5d6aeb9433199a8dd98f0d2bcaf263b
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-02-14 15:31:36 +0200

    Update OrderNotFoundException.java

--- src/main/java/consumer/products/server/order/OrderNotFoundException.java
+++ src/main/java/consumer/products/server/order/OrderNotFoundException.java
@@ -2,7 +2,7 @@ package consumer.products.server.order;
 
 public class OrderNotFoundException extends RuntimeException{
     public OrderNotFoundException(Long id) {
-        super("Order corresponding to id = " + id + " dose not exist" + ResponseEntity.notFound());
+        super("Order corresponding to id = " + id + " dose not exist");
     }
 
 }
